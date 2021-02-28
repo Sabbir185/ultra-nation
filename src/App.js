b6b4a85520f0a4767783719c9124e22a697bc023
unknown <sabbir.cse@yahoo.com>
2021-02-28 10:00:07 +0600

    data retrive by api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [country, setCountry] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountry(data));
+  },[]);
   return (
     <div className="App">
+      <p>Total country : {country.length}</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
